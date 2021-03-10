d8c1172794215b005a276b0c6e01e57811408d64
Jacob Marshall <jacob.marshall.jem@gmail.com>
2021-03-10 14:55:51 +0000

    Initial commit

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "title": "Desica status",
+  "name": "desica_status",
+  "description": "Welcome to Desica's home for status updates and maintenance.",
+  "defaultLocale": "en",
+  "locales": [
+    {
+      "code": "en",
+      "iso": "en-US",
+      "name": "English",
+      "file": "en.json"
+    }
+  ],
+  "content": {
+    "frontMatterFormat": "json"
+  }
+}
